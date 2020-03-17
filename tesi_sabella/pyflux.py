7c8fdba1b0dcc57c41f787918f133e84c91b693b
samuelesabella <samueleunipi@gmail.com>
2020-03-17 11:59:26 +0100

    New operators

--- tesi_sabella/pyflux.py
+++ tesi_sabella/pyflux.py
@@ -21,9 +21,15 @@ class FluxResponse():
     def __init__(self, res, query, groupby=True):
         self.query = query
         csv_data = self.parse_csv(res)
+        # TODO: handle empty queries and errors!
         keys = csv_data[3][3:]
         dtypes = csv_data[0][3:]
-        table = [x[3:] for x in csv_data[4:-1]]
+        # Parsing results
+        table = []
+        for x in csv_data[4:-1]:
+            if x==[]:
+                break
+            table.append(x[3:])
         dframe = pd.DataFrame(table, columns=keys)
         # Casting types ..... #
         for (k, dtype) in zip(keys, dtypes):
@@ -43,7 +49,6 @@ class FluxResponse():
             return pd.to_datetime(s, format='%Y-%m-%dT%H:%M:%S.%fZ')
         return pd.to_datetime(s, format='%Y-%m-%dT%H:%M:%SZ')
 
-    
     @staticmethod
     def castFluxSeries(pd_series, str_fluxtype):
         if str_fluxtype in FLUX_TYPE_MAP:
@@ -74,6 +79,12 @@ class FluxQueryFrom():
     def range(self, start, stop=None):
         if not stop:
             stop = 'now()'
+
+        if isinstance(start, pd.Timestamp):
+            start = start.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+        if isinstance(stop, pd.Timestamp):
+            stop = stop.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+
         self.query.append(f'range(start:{start}, stop:{stop})')
         return self
 
@@ -119,6 +130,22 @@ class FluxQueryFrom():
     def keep(self, columns):
         self.query.append(f'keep(columns:{columns})')
         return self
+    
+    def sort(self, columns, desc=False):
+        self.query.append(f'sort(columns: {columns}, desc: {str(desc).lower()})')
+        return self
+
+    def last(self):
+        self.query.append('last()')
+        return self
+
+    def first(self):
+        self.query.append('first()')
+        return self
+
+    def limit(self, n=10, offset=0):
+        self.query.append(f'limit(n: {n}, offset: {offset})')
+        return self
 
     def __str__(self):
         s = " |> ".join(self.query)
@@ -154,6 +181,14 @@ class Flux():
         q.filter(f'(r) => r._measurement == "{from_measurement}"')
         q.group([with_key]).distinct(with_key)
         return self(q)
+
+    def bucket_timerange(self, bucket):
+        # Returns the oldest and the newest value in the bucket
+        qfirst = FluxQueryFrom(bucket).range('1970-01-01T00:00:00Z').first() 
+        qlast = FluxQueryFrom(bucket).range('1970-01-01T00:00:00Z').last()
+        fs = self(qfirst, False).dframe.iloc[0]["_time"]
+        ls = self(qlast, False).dframe.iloc[0]["_time"]
+        return (fs, ls)
 
     def show_measurements(self, bucket, trange='-48h'):
         q = FluxQueryFrom(bucket)
