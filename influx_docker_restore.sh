0a8e5ad90d953e21cee880219bd0257e9d803422
samuelesabella <samueleunipi@gmail.com>
2020-03-21 10:43:08 +0100

    bug fix

--- influx_docker_restore.sh
+++ influx_docker_restore.sh
@@ -10,7 +10,7 @@ config="$config \\\n[coordinator]\\\n query-timeout = \\\"0\\\" \\\n log-queries
 
 docker run --rm -it -p 8086:8086 --entrypoint /bin/bash \
   -v $1:/backups influxdb:latest \
-  -c "(echo -e $config >> /etc/influxdb/influxdb.conf && cat /etc/influxdb/influxdb.conf && influxd) & 
+  -c "(echo -e $config >> /etc/influxdb/influxdb.conf && cat /etc/influxdb/influxdb.conf && influxd > /dev/null 2>&1) & 
       (sleep 2 && influxd restore -portable /backups && influx -type=$qlang)"
 
 
