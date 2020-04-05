c99df2c833eb18a57b0907fcf8d8a5b72c801cd5
samuelesabella <samueleunipi@gmail.com>
2020-04-05 10:46:01 +0200

    Minor restore changes

--- influx_docker_restore.sh
+++ influx_docker_restore.sh
@@ -12,6 +12,6 @@ docker run --rm -it -p 8086:8086 --entrypoint /bin/bash \
   --name tesi_sabella_influx \
   -v $1:/backups influxdb:latest \
   -c "(echo '$config' > /etc/influxdb/influxdb.conf && cat /etc/influxdb/influxdb.conf && influxd) & 
-      (sleep 2 && influxd restore -portable /backups && influx -type=$qlang)"
+      (sleep 2 && influxd restore -portable /backups && tail -f /dev/null)"
 
 
