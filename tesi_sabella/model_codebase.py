ba77f0dcf72155ce5e7ed04734c1efeb80151427
samuelesabella <samueleunipi@gmail.com>
2020-04-22 19:06:46 +0200

    Add contextual coherency

--- tesi_sabella/model_codebase.py
+++ tesi_sabella/model_codebase.py
@@ -23,45 +23,56 @@ class RNTrunc():
 zero_one_normal = RNTrunc(.5, .2, (0, 1))
 
 
-def coherency_generator(x, idx, time_windows, wlen, step_size):
+def coherency_generator(x, idx, time_windows):
     r = random.random()
-    if r < .25:
+    if r < .25: # Sample from the current context
         return (x, COH_TENSOR)
-    if r < .5:
-        x_b = time_windows[idx + (wlen / step_size)]
+    if r < .5: # Sample from the context of the next activity
+        x_b = time_windows[idx + 1]
         return (x_b, COH_TENSOR)
-    if r < .75:
-        random_shift = random.randint(1, len(time_windows))
-        x_b = time_windows[idx + (wlen / step_size) + random_shift]
+    if r < .75: # Sample from context distant in time
+        random_shift = random.randint(1, len(time_windows)-idx)
+        if random.random() > .5:
+            random_shift = random.randint(-2, -idx) 
+        x_b = time_windows[idx + 1 + random_shift]
         return (x_b, INC_TENSOR)
-    return (None, INC_TENSOR)
+    return (None, INC_TENSOR) # Sample from full dataset
+
 
 def random_sublist(l, sub_wlen):
     r = random.randint(0, len(l)-sub_wlen)
     return l[r:r+sub_wlen]
     
-def merge_b(x_a, x_b):
+
+def context_merge(ctx_a, ctx_b):
+    # TODO: Multivariate merge
     r = zero_one_normal()
-    r_idx = int(len(x_a) * r)
-    return (x_a[:r_idx] + x_b[r_idx:])
+    r_idx = int(len(ctx_a) * r)
+    return (ctx_a[:r_idx] + ctx_b[r_idx:])
 
 
-def ts_windowing(df, w_minutes=15, sub_w_minutes=7, overlap=.75):
+def ts_windowing(df, w_minutes=15, sub_w_minutes=7):
     X = []
     wlen = int(w_minutes * 4) # Samples per minutes (one sample every 15 seconds)
     sub_wlen = int(sub_w_minutes * 4)
-    step_size = max(int(wlen * (1-overlap)), 1)
     for _, ts in df.groupby(level=['category', 'host']):
-        wnds = list(mit.windowed(ts, wlen, step_size))
-        sub_wnds = map(lambda x: random_sublist(x, sub_wlen), wnds)
-        coherency_tuples = list(map(lambda v: coherency_generator(*v, wnds, wlen, step_size), enumerate(wnds)))
-        coherency_wnds, coherency_label = zip(*coherency_tuples)
-        X.append(sub_wnds, wnds, coherency_wnds, coherency_label)
+        ctx_wnds = list(mit.windowed(ts, wlen, sub_wlen))
+        activities_wnd = map(lambda x: random_sublist(x, sub_wlen), ctx_wnds)
+        coherency_tuples = map(lambda v: coherency_generator(*v, ctx_wnds), enumerate(ctx_wnds))
+        # adding samples
+        h_samples = zip(ctx_wnds, activities_wnd, coherency_tuples)
+        for activity, ctx, (coherency_ctx, coherency_label) in h_samples:
+            X.append({
+                "activity": activity, 
+                "context": ctx, 
+                "coherency_ctx": coherency_ctx, 
+                "coherency_label": coherency_label})
 
     def choice_n_merge(x):
-        if x[2] is None:
-            x[2] = random.choice(X)[1] 
-        return (x[0], x[1], merge_b(x[1], x[2]))
+        if x["coherency_ctxs"] is None:
+            x["coherency_ctxs"] = random.choice(X)["context"] 
+        merge = context_merge(x["context"], x["coherency_ctxs"])
+        return (x["activity"], x["context"], merge, x["label"])
     X = map(choice_n_merge, X)
 
     return X
@@ -74,10 +85,11 @@ class Contextual_Coherency():
         self.alpha = alpha
 
     def __call__(self, x, _):
-        sub_wnds, wnds, merged_coh, coh_label = x
-        l_ctx = torch.norm(sub_wnds - wnds, 2)
-        l_coh = F.binary_cross_entropy(merged_coh, coh_label)
-        ctx_coh = (alpha * l_ctx) + ((1 - self.alpha) * l_coh)
+        activity, context, merged_context, merge_label = x
+        context_loss = torch.norm(activity - context, 2)
+        coherency_loss = F.binary_cross_entropy(merged_context, merge_label)
+        ctx_coh = (self.alpha * context_loss) + ((1 - self.alpha) * coherency_loss)
+        return ctx_coh 
 
 
 # ----- ----- MODEL ----- ----- #
