af68d61efcb4cf86bfd39cc686e78f2ed2a9d146
samuelesabella <samueleunipi@gmail.com>
2020-04-24 18:17:02 +0200

    refactoring cicids2017 generator

---
+++ tesi_sabella/cicids2017_data_generator.py
@@ -0,0 +1,127 @@
+import pandas as pd
+from collections import defaultdict
+from data_generator import FluxDataGenerator, preprocessing 
+
+
+# ----- ----- PREPROCESSING ----- ----- #
+# ----- ----- ------------- ----- ----- #
+ATTACKS = [
+  # Tuesday
+  ["Brute Force",  "FTP-Patator",      "2017-07-03T09:20:00", "2017-07-03T10:20:00", "192.168.10.50"],
+  ["Brute Force",  "SSH-Patator",      "2017-07-03T14:00:00", "2017-07-03T15:00:00", "192.168.10.50"],
+  # Wednesday
+  ["DoS",          "slowloris",        "2017-07-04T09:47:00", "2017-07-04T10:10:00", "192.168.10.50"],
+  ["DoS",          "Slowhttptest",     "2017-07-04T10:14:00", "2017-07-04T10:35:00", "192.168.10.50"],
+  ["DoS",          "Hulk",             "2017-07-04T10:43:00", "2017-07-04T11:00:00", "192.168.10.50"],         
+  ["DoS",          "GoldenEye",        "2017-07-04T11:10:00", "2017-07-04T11:23:00", "192.168.10.50"],         
+  ["Side_channel", "Heartbleed",       "2017-07-05T15:12:00", "2017-07-05T15:32:00", "192.168.10.51"],
+  # Thursday
+  ["Web Attack",    "Brute Force",     "2017-07-06T9:20:00",  "2017-07-06T10:00:00", "192.168.10.50"],
+  ["Web Attack",    "XSS",             "2017-07-06T10:15:00", "2017-07-06T10:35:00", "192.168.10.50"],
+  ["Web Attack",    "Sql Injection",   "2017-07-06T10:40:00", "2017-07-06T10:42:00", "192.168.10.50"],
+  ["Infiltration",  "Meta exploit",    "2017-07-06T14:19:00", "2017-07-06T14:21:00", "192.168.10.8"],
+  ["Infiltration",  "Meta exploit",    "2017-07-06T14:33:00", "2017-07-06T14:35:00", "192.168.10.8"],
+  ["Infiltration",  "Cool disk",       "2017-07-06T14:53:00", "2017-07-06T15:00:00", "192.168.10.25"],         
+  ["Infiltration",  "Nmap - Portscan", "2017-07-06T15:04:00", "2017-07-06T15:45:00", "*"],
+  # Friday
+  ["Botnet",        "ARES",            "2017-07-07T10:02:00", "2017-07-07T11:02:00", "192.168.10.15"],
+  ["Botnet",        "ARES",            "2017-07-07T10:02:00", "2017-07-07T11:02:00", "192.168.10.9"],
+  ["Botnet",        "ARES",            "2017-07-07T10:02:00", "2017-07-07T11:02:00", "192.168.10.14"],
+  ["Botnet",        "ARES",            "2017-07-07T10:02:00", "2017-07-07T11:02:00", "192.168.10.5"],
+  ["Botnet",        "ARES",            "2017-07-07T10:02:00", "2017-07-07T11:02:00", "192.168.10.8"],
+  ["DDoS",          "LOIT",            "2017-07-07T15:56:00", "2017-07-07T16:16:00", "192.168.10.50"],
+  ["Port Scan",     "firewall on",     "2017-07-07T13:55:00", "2017-07-07T13:57:00", "192.168.10.50"],
+  ["Port Scan",     "firewall on",     "2017-07-07T13:58:00", "2017-07-07T14:00:00", "192.168.10.50"],
+  ["Port Scan",     "firewall on",     "2017-07-07T14:01:00", "2017-07-07T14:04:00", "192.168.10.50"],
+  ["Port Scan",     "firewall on",     "2017-07-07T14:05:00", "2017-07-07T14:07:00", "192.168.10.50"],
+  ["Port Scan",     "firewall on",     "2017-07-07T14:08:00", "2017-07-07T14:10:00", "192.168.10.50"],
+  ["Port Scan",     "firewall on",     "2017-07-07T14:11:00", "2017-07-07T14:13:00", "192.168.10.50"],
+  ["Port Scan",     "firewall on",     "2017-07-07T14:14:00", "2017-07-07T14:16:00", "192.168.10.50"],
+  ["Port Scan",     "firewall on",     "2017-07-07T14:17:00", "2017-07-07T14:19:00", "192.168.10.50"],
+  ["Port Scan",     "firewall on",     "2017-07-07T14:20:00", "2017-07-07T14:21:00", "192.168.10.50"],
+  ["Port Scan",     "firewall on",     "2017-07-07T14:22:00", "2017-07-07T14:24:00", "192.168.10.50"],
+  ["Port Scan",     "firewall on",     "2017-07-07T14:33:00", "2017-07-07T14:33:00", "192.168.10.50"],
+  ["Port Scan",     "firewall on",     "2017-07-07T14:35:00", "2017-07-07T14:35:00", "192.168.10.50"],
+  ["Port Scan",     "NMap sS",         "2017-07-07T14:51:00", "2017-07-07T14:53:00", "192.168.10.50"],
+  ["Port Scan",     "NMap sT",         "2017-07-07T14:54:00", "2017-07-07T14:56:00", "192.168.10.50"],
+  ["Port Scan",     "NMap sF",         "2017-07-07T14:57:00", "2017-07-07T14:59:00", "192.168.10.50"],
+  ["Port Scan",     "NMap sX",         "2017-07-07T15:00:00", "2017-07-07T15:02:00", "192.168.10.50"],
+  ["Port Scan",     "NMap sN",         "2017-07-07T15:03:00", "2017-07-07T15:05:00", "192.168.10.50"],
+  ["Port Scan",     "NMap sP",         "2017-07-07T15:06:00", "2017-07-07T15:07:00", "192.168.10.50"],
+  ["Port Scan",     "NMap sV",         "2017-07-07T15:08:00", "2017-07-07T15:10:00", "192.168.10.50"],
+  ["Port Scan",     "NMap sU",         "2017-07-07T15:11:00", "2017-07-07T15:12:00", "192.168.10.50"],
+  ["Port Scan",     "NMap sO",         "2017-07-07T15:13:00", "2017-07-07T15:15:00", "192.168.10.50"],
+  ["Port Scan",     "NMap sA",         "2017-07-07T15:16:00", "2017-07-07T15:18:00", "192.168.10.50"],
+  ["Port Scan",     "NMap sW",         "2017-07-07T15:19:00", "2017-07-07T15:21:00", "192.168.10.50"],
+  ["Port Scan",     "NMap sR",         "2017-07-07T15:22:00", "2017-07-07T15:24:00", "192.168.10.50"],
+  ["Port Scan",     "NMap sL",         "2017-07-07T15:25:00", "2017-07-07T15:25:00", "192.168.10.50"],
+  ["Port Scan",     "NMap sI",         "2017-07-07T15:26:00", "2017-07-07T15:27:00", "192.168.10.50"],
+  ["Port Scan",     "NMap b" ,         "2017-07-07T15:28:00", "2017-07-07T15:29:00", "192.168.10.50"]
+]
+    
+
+def label(df):                                          
+    def to_dt(x):
+        return pd.to_datetime(x, format='%Y-%m-%dT%H:%M:%S.%f')
+
+    df["status"] = "normal"
+    host_idxs = df.index.get_level_values("host")
+    times_idxs = df.index.get_level_values("_time")
+    
+    for atype, adetail, start, stop, host in ATTACKS:
+        dt_start = to_dt(start)
+        dt_stop = to_dt(stop)
+        if host == "*":
+            host_selector = True
+        else:
+            host_selector = (host_idxs==host)
+        time_selector = (times_idxs > dt_start) & (times_idxs < dt_stop)
+        df.loc[host_selector & time_selector, "status"] = f"{atype}__{adetail}"
+    return df
+
+def cicids2017_preprocessing(df):
+    # Filtering hosts ..... #
+    df = df[df.index.get_level_values('host').str.contains("192.168.10.")]
+    # Passing the ball ..... #
+    preproc_df = preprocessing(df)
+    # Removing initial non zero traffic ..... #
+    index_hours = preproc_df.index.get_level_values("_time").hour
+    working_hours = (index_hours > 8) & (index_hours < 17)
+    preproc_df = preproc_df[working_hours]
+
+    return label(preproc_df)
+
+
+# ----- ----- CICIDS2017 ----- ----- #
+# ----- ----- ---------- ----- ----- #
+CICIDS2017_IPV4_NETMAP = defaultdict(lambda : "unknown device class", {
+    "192.168.10.3": "server",
+    "192.168.10.50": "server",
+    "192.168.10.51": "server",
+    "205.174.165.68": "server",
+    "205.174.165.66": "server", 
+
+    "192.168.10.19": "pc",
+    "192.168.10.17": "pc",
+    "192.168.10.16": "pc",
+    "192.168.10.12": "pc",
+    "192.168.10.9": "pc",
+    "192.168.10.5": "pc",
+    "192.168.10.8": "pc",
+    "192.168.10.14": "pc",
+    "192.168.10.15": "pc",
+    "192.168.10.25": "pc",
+})
+
+
+class CICIDS2017(FluxDataGenerator):
+    def __init__(self, *args):
+        super(CICIDS2017, self).__init__(*args, None)
+
+    @staticmethod
+    def pre_processing(df):
+        df = super().pre_processing(df)
+        return 
+
+    def category_map(self, new_samples):
+        return new_samples['host'].map(CICIDS2017_IPV4_NETMAP)
