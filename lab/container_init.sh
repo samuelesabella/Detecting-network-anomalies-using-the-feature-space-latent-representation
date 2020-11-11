d904e647c62e75424fa08e5409ab4c972783e55a
samuelesabella <samueleunipi@gmail.com>
2020-11-11 19:24:40 +0100

    Influx lab fix

--- lab/container_init.sh
+++ lab/container_init.sh
@@ -10,7 +10,14 @@ TIMESHIFT_FILE="/app/ext/timeshift.txt"
 LOCALNET="192.168.10.0/24,205.174.165.0/24"
 
 redis-server 1>/dev/null &
-service influxdb start
+mv /app/tesi_sabella/lab/influxd_config /etc/influxdb/influxdb.conf
+influxd &
+
+eval $(ssh-agent) && \
+ssh-add /app/keys/github_key && \
+ssh-keyscan -H github.com >> /etc/ssh/ssh_known_hosts && \
+cd /app/tesi_sabella && git pull && cd -
+pip3 install -r /app/tesi_sabella/lab.requirements.txt 
 
 # Starting dummy interface ..... #
 ip link add fake_nic type dummy && \
@@ -25,22 +32,23 @@ curl -sX POST -d "timeseries_driver=influxdb&ts_post_data_url=http%3A%2F%2Flocal
 echo "> Environment ready"
 # Formatting timeshift file ..... #
 echo "TCPREPLAY_START = {" > $TIMESHIFT_FILE
-
+tcpreplay -i fake_nic "$absfname" &
+cat
 # Starting tcpreplay
-for absfname in /app/pcaps/*.pcap; do
-    [ -f "$absfname" ] || break
-    fname=`basename "$absfname" .pcap`
-    echo "> Replaying $fname"
-
-    python3.7 /app/tesi_sabella/src/data_generator.py -b ntopng --credentials=admin:admin -o /app/ext/$fname -e 2 &
-    data_gen_pid=$!
-
-    start=`date --utc +"%Y-%m-%dT%H:%M:%S.%N"`
-    tcpreplay -i fake_nic "$absfname"
-    echo " \"$fname\": \"$start\", " >> $TIMESHIFT_FILE
-
-    kill -INT data_gen_pid
-    influxd backup -portable -database ntopng "/app/ext/${fname}.backup"
-done
-
-echo "}" >> $TIMESHIFT_FILE
+# for absfname in /app/pcaps/*.pcap; do
+#     [ -f "$absfname" ] || break
+#     fname=`basename "$absfname" .pcap`
+#     echo "> Replaying $fname"
+# 
+#     python3.7 /app/tesi_sabella/src/data_generator.py -b ntopng --credentials=admin:admin -o /app/ext/$fname -e 2 &
+#     data_gen_pid=$!
+# 
+#     start=`date --utc +"%Y-%m-%dT%H:%M:%S.%N"`
+#     tcpreplay -i fake_nic "$absfname"
+#     echo " \"$fname\": \"$start\", " >> $TIMESHIFT_FILE
+# 
+#     kill -INT data_gen_pid
+#     influxd backup -portable -database ntopng "/app/ext/${fname}.backup"
+# done
+# 
+# echo "}" >> $TIMESHIFT_FILE
