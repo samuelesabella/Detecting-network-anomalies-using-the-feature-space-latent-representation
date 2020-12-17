60d67b962370378c454c0c2be50fbf244bf1668d
samuelesabella <samueleunipi@gmail.com>
2020-12-17 19:15:16 +0100

    Encoder decoder network

--- src/model_codebase.py
+++ src/model_codebase.py
@@ -93,16 +93,20 @@ class Contextual_Coherency():
     @staticmethod
     def as_score(net, X):
         with torch.no_grad():
-            e_actv, e_ap, e_an = net.forward(X)
-        ap_dist = F.relu(torch.norm((e_actv - e_ap), p=2, dim=1) - BETA_1)
-        an_dist = F.relu(BETA_2 - torch.norm((e_actv - e_an), p=2, dim=1))
-        return torch.mean(ap_dist + an_dist)
+            mout = net.forward(X)
+        return self(mout)
 
     def __call__(self, model_out, _=None):
         e_actv, e_ap, e_an = model_out 
         ap_dist = F.relu(torch.norm((e_actv - e_ap), p=2, dim=1) - BETA_1)
         an_dist = F.relu(BETA_2 - torch.norm((e_actv - e_an), p=2, dim=1))
         return torch.mean(ap_dist + an_dist)
+
+class ReconstructionError():
+    def __call__(self, model_out, target):
+        pointwise_mse = torch.sum(torch.square(model_out - target) , axis=2) # Error of each point prediction
+        batch_mse = torch.sum(pointwise_mse) # Sum of error of each prediction
+        return torch.mean(batch_mse)
 
 
 # ----- ----- TUPLE MINING ----- ----- #
@@ -308,4 +312,55 @@ class STC(AnchorTs2Vec):
         rnn_out, _ = self.rnn(x)
         e = self.embedder(rnn_out[:, -1])
         return e
+
+
+
+# ----- ----- MODELS ----- ----- #
+# ----- ----- ------ ----- ----- #
+class AE(torch.nn.Module):
+    def __init__(self, input_size=None, sigma=.0, latent_size=64, rnn_layers=64, teacher_forcing_ratio=1.):
+        super(AE, self).__init__()
+        self.teacher_forcing_ratio = teacher_forcing_ratio
+        self.sigma = sigma
+        self.hidden_size = latent_size
+
+        self.encoder = nn.GRU(input_size=input_size, hidden_size=latent_size, num_layers=rnn_layers, batch_first=True)
+        self.decoder = nn.GRU(input_size=input_size, hidden_size=latent_size, num_layers=rnn_layers, batch_first=True)
+        self.out = nn.Linear(latent_size, input_size)
+
+    def encoder_init_hidden(self, batch_size):
+        return torch.zeros(1, batch_size, self.hidden_size)
+
+    def toembedding(self, x):
+        rnn_out, _ = self.rnn(x)
+        e = self.embedder(rnn_out[:, -1])
+        return e
+
+    def context_anomaly(self, ctx):
+        return NotImplementedError
+
+    def forward(self, X): 
+        init_h = self.encoder_init_hidden(X.size(0))
+        lstm_out, h_n = self.encoder(X, init_h)
+
+        decoder_input = torch.zeros_like(X[:, 0:1, :]) # batch_size, sequence_len, features
+        decoder_hidden = h_n
+        
+        x_tilde = torch.Tensor([])
+        use_teacher_forcing = True if random.random() < self.teacher_forcing_ratio else False
+        for i in range(X.size(1)): # sequence_len
+            if use_teacher_forcing:
+                decoder_output, decoder_hidden = self.decoder(decoder_input, decoder_hidden)
+                decoder_input = X[:, i:i+1, :]
+            else:
+                decoder_output, decoder_hidden = self.decoder(decoder_input, decoder_hidden)
+                decoder_input = decoder_output.detach()
+            xi_tilde = self.out(decoder_output.squeeze()).unsqueeze(1)
+            x_tilde = torch.cat([x_tilde, xi_tilde], axis=1)
+        return x_tilde
+
+
+
+
+
 
